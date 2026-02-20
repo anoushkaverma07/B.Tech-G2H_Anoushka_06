e178155b8b88ca113c993b6ff25391ac15b2d705
anoushkaverma07 <anoushkaverma17@gmail.com>
2026-02-20 11:29:50 +0530

    Add files via upload
    To find the greatest of three numbers

---
+++ Greatest number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+   int a,b,c;
+   printf("Enter value of a: ");
+   scanf("%d",&a);
+   printf("Enter value of b: ");
+   scanf ("%d",&b);
+   printf("Enter value of c: ");
+   scanf("%d",&c);
+   if (a>b && a>c)
+   {
+      printf("%d is the greatest no.",a);
+   }
+   else if (b>a && b>c)
+   {
+      printf("%d is the greatest no.",b);
+   }
+   else
+   {
+      printf("%d is the greatest no.",c);
+   }
+   return 0;
+}
